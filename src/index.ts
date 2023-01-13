609350261040c209d3c0d3ac571c2d6a4432a2fe
biavarejao <bia.varejao@usp.br>
2023-01-13 08:05:33 -0300

    lint and prettier updates

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,33 @@
-import { ApolloServer } from '@apollo/server';
+import { ApolloServer } from "@apollo/server";
 
-import { startStandaloneServer } from '@apollo/server/standalone';
+import { startStandaloneServer } from "@apollo/server/standalone";
 
 const typeDefs = `
 
   type Query {
     hello: String
   }
-`
+`;
 
-
-  // The root provides a resolver function for each API endpoint
+// The root provides a resolver function for each API endpoint
 const resolvers = {
-    Query: {
-      hello: () => {
-      return 'Hello world!';
-      },
+  Query: {
+    hello: () => {
+      return "Hello world!";
     },
-  };
+  },
+};
 // The ApolloServer constructor requires two parameters: your schema
 
 // definition and your set of resolvers.
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers
+  typeDefs,
+  resolvers,
 });
 
-  
 startStandaloneServer(server, {
-
-    listen: { port: 4000 },
-  
-  }).then( ({url})=>{
-  
-  
-  console.log(`🚀  Server ready at: ${url}`);})
+  listen: { port: 4000 },
+}).then(({ url }) => {
+  console.log(`🚀  Server ready at: ${url}`);
+});
