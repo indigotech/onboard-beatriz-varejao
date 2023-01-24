7b64fa84f6c6c17876d435024eb9c6d50fe1bfce
biavarejao <bia.varejao@usp.br>
2023-01-24 10:04:48 -0300

    correcting

--- src/test/index.ts
+++ src/test/index.ts
@@ -1,7 +1,7 @@
 import { setupServer } from '../server';
 import { clearDatabase, setupDatabase } from '../database';
 import axios from 'axios';
-import chai from 'chai';
+import { expect } from 'chai';
 import * as dotenv from 'dotenv';
 import { user1, expectedResponse, queryUser, userDatabase } from './input';
 
@@ -15,17 +15,17 @@ describe('Testes', () => {
     const url = 'http://localhost:4000/';
     const query = 'query { hello }';
     const response = await axios.post(url, { query });
-    chai.expect(response.data).to.eql({ data: { hello: 'Hello world!' } });
+    expect(response.data).to.eql({ data: { hello: 'Hello world!' } });
   });
   it('creating user', async () => {
     const url = 'http://localhost:4000';
     const response = await axios.post(url, { query: user1 });
-    chai.expect(response.data).to.eql(expectedResponse);
+    expect(response.data).to.eql(expectedResponse);
   });
   it('checking database', async () => {
     const url = 'http://localhost:4000';
     const response = await axios.post(url, { query: queryUser });
-    chai.expect(response.data).to.eql(userDatabase);
+    expect(response.data).to.eql(userDatabase);
   });
   after(async () => {
     await clearDatabase();
