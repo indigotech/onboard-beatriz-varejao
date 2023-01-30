97f6c44fbeac85c60b4b48f6b305a77e8e31689f
biavarejao <bia.varejao@usp.br>
2023-01-30 09:09:57 -0300

    correcting

--- src/test/index.ts
+++ src/test/index.ts
@@ -1,7 +1,7 @@
 import { setupServer } from '../server';
 import { clearDatabase, setupDatabase } from '../database';
 import axios from 'axios';
-import chai from 'chai';
+import { expect } from 'chai';
 import * as dotenv from 'dotenv';
 import { user1, expectedResponse, queryUser, userDatabase } from './input';
 
@@ -11,21 +11,21 @@ describe('Testes', () => {
     await setupDatabase();
     await setupServer();
   });
-  it('query hello', async () => {
+  it('should return Hello World', async () => {
     const url = 'http://localhost:4000/';
     const query = 'query { hello }';
     const response = await axios.post(url, { query });
-    chai.expect(response.data).to.eql({ data: { hello: 'Hello world!' } });
+    expect(response.data).to.eql({ data: { hello: 'Hello world!' } });
   });
   it('creating user', async () => {
     const url = 'http://localhost:4000';
     const response = await axios.post(url, { query: user1 });
-    chai.expect(response.data).to.eql(expectedResponse);
+    expect(response.data).to.eql(expectedResponse);
   });
   it('checking database', async () => {
     const url = 'http://localhost:4000';
     const response = await axios.post(url, { query: queryUser });
-    chai.expect(response.data).to.eql(userDatabase);
+    expect(response.data).to.eql(userDatabase);
   });
   after(async () => {
     await clearDatabase();
