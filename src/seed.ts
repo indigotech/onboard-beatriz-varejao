f36aee055caea60364023d40d70cc8ecef5c8b1e
biavarejao <73177791+biavarejao@users.noreply.github.com>
2023-02-03 12:11:02 -0300

    Update src/seed.ts
    Co-authored-by: leandro-faria <leandrocf_96@hotmail.com>

--- src/seed.ts
+++ src/seed.ts
@@ -82,16 +82,7 @@ function formatDate(date) {
   let m = (date.getUTCMonth() + 1).toString(); // getUTCMonth() returns 0 - 11
   const y = date.getUTCFullYear().toString(); // getUTCFullYear() returns a 4-digit year
   let formatted = '';
-  if (d.length === 1) {
-    // pad to two digits if needed
-    d = '0' + d;
-  }
-  if (m.length === 1) {
-    // pad to two digits if needed
-    m = '0' + m;
-  }
-  formatted = d + '/' + m + '/' + y; // concatenate for output
-  return formatted;
+  return = d.padStart(2, '0') + '/' + m.padStart(2, '0') + '/' + y; // concatenate for output
 }
 
 function float2int(value: number) {
